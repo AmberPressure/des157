a274ce8d4b13e3d924ec0e058f4f722fb19ffe89
Amber Kumar <ambkumar@ucdavis.edu>
2018-02-27 12:17:39 -0800

    changed the js for the splash page

--- placeholder.js
+++ placeholder.js
@@ -3,13 +3,32 @@ function setup (){
   var placeholder =createCanvas (655,180);
   placeholder.parent('placeholder');
   background(169,169,169);
+  strokeWeight(20.0);
+  stroke(255, 100);
 }
 
-function draw(){
-  if (mouseIsPressed){
-    fill(0);
-  }else{
-    fill(255);
-  }
-  ellipse(mouseX,mouseY, 20,20);
+var x = 100,
+  y = 100,
+  angle1 = 0.0,
+  segLength = 50;
+
+function draw() {
+  background(0);
+
+  dx = mouseX - x;
+  dy = mouseY - y;
+  angle1 = atan2(dy, dx);
+  x = mouseX - (cos(angle1) * segLength);
+  y = mouseY - (sin(angle1) * segLength);
+
+  segment(x, y, angle1);
+  ellipse(x, y, 20, 20);
+}
+
+function segment(x, y, a) {
+  push();
+  translate(x, y);
+  rotate(a);
+  line(0, 0, segLength, 0);
+  pop();
 }
